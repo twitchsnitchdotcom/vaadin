6b0b909ebb684deccb8cbf4b0012cebdf1d32a5d
horizondeep <jamie@twitchsnitch.com>
2022-04-26 13:24:30 +0200

    stuff

--- src/main/java/com/example/application/views/NFTView.java
+++ src/main/java/com/example/application/views/NFTView.java
@@ -12,6 +12,8 @@ import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import de.f0rce.viz.Viz;
+import de.f0rce.viz.VizFormat;
 
 import javax.annotation.security.PermitAll;
 import java.util.Arrays;
@@ -26,29 +28,21 @@ public class NFTView extends VerticalLayout {
         this.service = service;
         addClassName("dashboard-view");
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        add(getContactStats(), getCompaniesChart(), slider());
+        add(viz(), slider());
     }
 
-    private Component getContactStats() {
-        Span stats = new Span(service.countContacts() + " contacts");
-        stats.addClassNames("text-xl", "mt-m");
-        return stats;
+    private Component viz() {
+        Viz viz = new Viz(); // height: 300px - width: 600px
+        viz.setFormat(VizFormat.svg); // default
+        viz.setGraph("digraph H { a -> b }"); // simple dot notation
+        return viz;
     }
 
-    private Chart getCompaniesChart() {
-        Chart chart = new Chart(ChartType.PIE);
-
-        DataSeries dataSeries = new DataSeries();
-        service.findAllCompanies().forEach(company ->
-            dataSeries.add(new DataSeriesItem(company.getName(), company.getEmployeeCount())));
-        chart.getConfiguration().setSeries(dataSeries);
-        return chart;
-    }
 
     private Splide slider(){
-        ImageSlide slide1 = new ImageSlide("images/slide_1.jpg");
-        ImageSlide slide2 = new ImageSlide("images/slide_2.png");
-        ImageSlide slide3 = new ImageSlide("https://source.unsplash.com/random/1000x1000?sig=3");
+        ImageSlide slide1 = new ImageSlide("https://lh3.googleusercontent.com/z5ZYxxEMXyYZcnFbDnYg5HEMOUKUIvmgxp89ohL4NVHYomoa5PIS56JPg3FFSQ1DYb_rTlemD8RLsNBzYW1SFoG6L5C4DlC3osAP=w335");
+        ImageSlide slide2 = new ImageSlide("https://lh3.googleusercontent.com/fRREGr2ADoIG1FILZjEBoaWb2A6FpJDHebH0xM9P8OPgGfJ_eQiDnnJqtbb4zQVuQ0uPpUQxXTx2wfYZpqM_jRsk_nGuht_xNuHsKGU=w335");
+        ImageSlide slide3 = new ImageSlide("https://lh3.googleusercontent.com/HLlVT316u7fQRJX6h8solpLuELHlXfqdjAioS2aPvZcNf4G_MIAMgsVgmbUVt9aFyoYh1x5LmJPHacqx3FYdnq-nQoL5KRKdYgN2NA=w335");
 
         Splide slider = new Splide(Arrays.asList(slide1, slide2, slide3));
         slider.setId("images-slider-demo");
